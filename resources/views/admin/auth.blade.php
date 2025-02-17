16088cd927eec7cf0eb0415d9676f6817ca05be7
Syrym <suleimenov.syrym.s@gmail.com>
2025-02-17 16:54:09 +0500

    update view and route

--- resources/views/admin/auth.blade.php
+++ resources/views/admin/auth.blade.php
@@ -3,9 +3,11 @@
     <head>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta charset="utf-8">
-        @Vite(['resources/js/auth.js','resources/css/auth.css'])
+        <!-- @Vite(['resources/js/auth.js','resources/css/auth.css']) -->
         <title>Вход на страницу</title>
         <link rel="stylesheet" href="https://cdn.webix.com/edge/webix.css">
+        <link rel="stylesheet" href={{ asset("/build/assets/auth-cRNMswxZ.css") }}>
+        <script src={{ asset("/build/assets/auth-BV2NMjge.js") }}></script>
     </head>
     <body>
         <div id="auth" class="flex-box"></div>
