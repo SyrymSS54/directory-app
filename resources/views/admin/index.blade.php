0966e40b9eea5e607dc96c7e1e15ca060b745467
Syrym <suleimenov.syrym.s@gmail.com>
2025-02-17 19:20:35 +0500

    Решение проблемы

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <title>Страница Админа</title>
         <link rel="stylesheet" href={{ asset("/build/assets/admin-Dfdgq3XR.css") }}>
-        <script type="module" src={{ asset("/build/assets/auth-BV2NMjge.js") }}></script>
+        <script type="module" src={{ asset("/build/assets/admin-CsBudn-N.js") }}></script>
         <script type="module" src={{ asset("/build/assets/webix-WWAde2qZ.js") }}></script>
         <link rel="stylesheet" href="https://cdn.webix.com/edge/webix.css">
         <link rel="stylesheet" href="//cdn.webix.com/materialdesignicons/5.8.95/css/materialdesignicons.min.css" type="text/css" charset="utf-8">
